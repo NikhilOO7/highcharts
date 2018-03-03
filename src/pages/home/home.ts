43d02e88fac1408952e5a24ae4dc7662fe26e827
NikhilOO7 <NikhilOO7@users.noreply.github.com>
2018-03-03 13:25:33 +0530

    High charts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,7 +7,36 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+	chartOptions : any;
+
   constructor(public navCtrl: NavController) {
+
+  	this.chartOptions = {
+  		chart: {
+            type: 'bar'
+        },
+        title: {
+            text: 'Fruit Consumption',
+            style: {
+            	color : "#ff0000"
+            }
+        },
+        xAxis: {
+            categories: ['Apples', 'Bananas', 'Oranges']
+        },
+        yAxis: {
+            title: {
+                text: 'Fruit eaten'
+            }
+        },
+        series: [{
+            name: 'Jane',
+            data: [1, 0, 4]
+        }, {
+            name: 'John',
+            data: [5, 7, 3]
+        }]
+  	}
 
   }
 
